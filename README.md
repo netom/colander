32fdabcf249dfde4aabbf9503485e3b4a74ae47f
Fábián Tamás László <giganetom@gmail.com>
2015-03-15 17:50:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Colander
+==
+
+Colander is an input filter and validator library for PHP
+with a functional twist.
+
---
+++ colander.php
@@ -0,0 +1,248 @@
+<?php
+
+namespace colander;
+
+class FactoryException extends \Exception {}
+
+class ValidationException extends \Exception {}
+
+/*
+ * If the first parameter is false, throw exception
+ */
+function trueOrX($bool, $errmsg) {
+    if (!$bool) throw new ValidationException($errmsg);
+}
+
+function _chkCallables($callables) {
+    if (!is_array( $callables ) && ! $callables instanceof Traversable ) {
+        throw new FactoryException(
+            'The seq combinator expects a Traversable or array of callables. ' .
+            'The given parameter is not Traversable, nor an array.'
+        );
+    }
+
+    foreach ($callables as $callable) {
+        if (!is_callable($callable, true)) {
+            throw new FactoryException(
+                'The seq combinator expects a Traversable of callables. ' .
+                'An element of the input list is not callable.'
+            );
+        }
+    }
+}
+
+/*
+ * Combine callables into a sequence
+ *
+ * Combine exceptions along the way
+ * 
+ * seq([f1, f2, f3])($d) = f3(f2(f1($d)))
+ */
+function seq($callables) {
+    _chkCallables($callables);
+
+    return function ($data) use ($callables) {
+        $ret = $data;
+
+        $exceptions = [];
+
+        foreach ($callables as $callable) {
+            try {
+                $ret = $callable($ret);
+            } catch (ValidationException $e) {
+                $exceptions[] = $e;
+            }
+        }
+
+        if (count($exceptions) > 0) {
+            throw new ValidationException(join(', ', array_map(function ($e) {
+                return $e->getMessage();
+            }, $exceptions)));
+        }
+
+        return $ret;
+    };
+}
+
+/*
+ * Combine callables into a sequence
+ *
+ * Throw exception immediately at the first problem
+ * 
+ * seq([f1, f2, f3])($d) = f3(f2(f1($d)))
+ */
+function seq_($callables) {
+    _chkCallables($callables);
+
+    return function ($data) use ($callables) {
+        $ret = $data;
+        foreach ($callables as $callable) {
+            $ret = $callable($ret);
+        }
+        return $ret;
+    };
+}
+
+/*
+ * Combine a map of validation functions into a map validator
+ * 
+ * map(['f1' => f1, 'f2' => f2], $d) = ['f1' => f1($d['f1'], 'f2' => f2($d['f2'])]
+ * 
+ * It returns fields with no filter rules unchanged.
+ */
+function map($callables) {
+    _chkCallables($callables);
+
+    return function ($data) use ($callables) {
+        $ret = $data;
+        foreach ($callables as $name => $callable) {
+            $ret[$name] = $callable($ret[$name]);
+        }
+        return $ret;
+    };
+}
+
+/*
+ * Combine a map of validation functions into a map validator
+ * 
+ * map(['f1' => f1, 'f2' => f2], $d) = ['f1' => f1($d['f1'], 'f2' => f2($d['f2'])]
+ * 
+ * Fields with no rules count as valudation errors.
+ */
+function map_() {
+    _chkCallables($map);
+
+    return function ($data) use ($map) {
+        $ret = $data;
+        foreach ($callables as $name => $callable) {
+            $ret[$name] = $callable($ret[$name]);
+        }
+        return $ret;
+    };
+}
+
+/*
+ * Combine a tuple of filter functions into a tuple filter
+ */
+function tuple() {
+}
+
+function tuple_() {
+}
+
+/*
+ * Call a callable with a single parameter
+ */
+function call($callable, $param) {
+    return call_user_func($callable, $param);
+}
+
+/*
+ * Generates and/or loads PHP code
+ * 
+ * Most of the filter / factory functions is just mindless
+ * boilerplate. This function takes care of that.
+ */
+function mkBolilerplate($bpdir) {
+
+    $phpname = $bpdir . '/yativafip_generated.php';
+
+    if (is_file($phpname)) {
+        //require_once $phpname;
+        //return;
+    }
+
+    $php = "<?php\n\nnamespace FV;\n\n";
+
+    /*
+     * PHP's is_* functions wrapped as filter functions
+     */
+
+    $a = [
+        'array',
+        'bool',
+        'callable',
+        'double',
+        'float',
+        'int',
+        'integer',
+        'long',
+        'null',
+        'numeric',
+        'object',
+        'real',
+        'resource',
+        'scalar',
+        'string'
+    ];
+
+    foreach ($a as $name) {
+        $filtname = 'is' . ucfirst($name);    // Filter function name
+        $factname = 'f' . ucfirst($filtname); // Factory function name
+        $pname    = 'is_' . $name;            // PHP function name
+
+        $_d = '$d';
+
+        $php .= 
+            "function $filtname($_d) {\n" .
+            "    trueOrX($pname($_d), 'not a $name');\n" .
+            "    return $_d;\n" .
+            "}\n" .
+            "function $factname() {\n" .
+            "    return function($_d) { return $filtname($_d); };\n" .
+            "}\n\n";
+    }
+
+    /*
+     * Our validation functions
+     */
+     
+    $a = [
+        'maxLen'   => ['strlen($d) <= $p', 'longer than $p'],
+        'minLen'   => ['strlen($d) >= $p', 'shorter than $p'],
+        'maxCount' => ['count($d) <= $p', 'longer than $p'],
+        'minCount' => ['count($d) >= $p', 'shorter than $p'],
+        'max'      => ['$d <= $p', 'greater than $p'],
+        'min'      => ['$d >= $p', 'smaller than $p']
+    ];
+    
+    foreach ($a as $name => list($cond, $errmsg)) {
+        $filtname = $name;
+        $factname = 'f' . ucfirst($name);
+
+        $_d = '$d';
+        $_p = '$p';
+
+        $php .= 
+            "function $filtname($_p, $_d) {\n" .
+            "    trueOrX($cond, '$errmsg');\n" .
+            "    return $_d;\n" .
+            "}\n" .
+            "function $factname($_p) {\n" .
+            "    return function($_d) use ($_p) { return $filtname($_p, $_d); };\n" .
+            "}\n\n";
+    }
+
+    file_put_contents($phpname, $php);
+
+    require_once $phpname;
+}
+
+/*************** USAGE *****************/
+
+mkBolilerplate('/tmp');
+
+$data_in = [
+    'name' => 'This is my name',
+    'number_of_pets' => 12
+];
+
+$data = call(
+    map([
+        'name' => seq([fIsString(), fMaxLen(128)]),
+        'number_of_pets' => seq([fIsInt(), fMax(5) ])
+    ]),
+    $data_in
+);
+
+var_dump($data);
